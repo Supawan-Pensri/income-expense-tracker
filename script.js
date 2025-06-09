7fa0d82724d8bff481590200c8f44d879600e591
supawan <supawanpensri@gmail.com>
2025-06-09 14:08:24 +0700

    add funtion

--- script.js
+++ script.js
@@ -1,50 +1,71 @@
-//อ้างอิง element in index.html
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
 
-const balance = document.getElementById('balance')
-const money_plus = document.getElementById('money-plus')
-const money_minus = document.getElementById('money-minus')
-const list = document.getElementById('list')
-const form = document.getElementById('form')
-const text = document.getElementById('text')
-const amount = document.getElementById('amount')
+let transactions = [];
 
-const dataTransaction = [
-    {
-        id: 1,
-        text: 'ค่าขนม',
-        amount: -100
-    },
-    {
-        id: 2,
-        text: 'ค่าห้อง',
-        amount: -3500
-    },
-    {
-        id: 3,
-        text: 'เงินเดือน',
-        amount: +18000
-    },
-    {
-        id: 4,
-        text: 'ค่าอาหาร',
-        amount: -300
-    }
-]
-const transactions=dataTransaction;
-
-function init(){
+function init() {
+    list.innerHTML = '';
     transactions.forEach(addDataToList);
-    
+    calculeteMoney();
 }
 
-function addDataToList(transactions){
-    const symbol = transactions.amount < 0 ? '-': '+';
-    const status = transactions.amount < 0 ? 'minus':'puls'
-    console.log(status);
-    const item = document.createElement('li')
-    item.classList.add(status)
-    item.innerHTML = `${transactions.text}<span>${symbol}${Math.abs(transactions.amount)}<span><button class="delete-btn">x</button>`;
-    list.appendChild(item)
+function addDataToList(transaction) {
+    const symbol = transaction.amount < 0 ? '-' : '+';
+    const status = transaction.amount < 0 ? 'minus' : 'puls';
+    const item = document.createElement('li');
+    const result = numberWithCommas(Math.abs(transaction.amount));
+    item.classList.add(status);
+    item.innerHTML = `${transaction.text}<span>${symbol}${result}</span><button class="delete-btn" onclick="removeData(${transaction.id})">x</button>`;
+    list.appendChild(item);
 }
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function autoID() {
+    return Math.floor(Math.random() * 1000000);
+}
+
+function calculeteMoney() {
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((result, item) => (result += item), 0).toFixed(2);
+    const income = amounts.filter(item => item > 0).reduce((result, item) => (result += item), 0).toFixed(2);
+    const expense = amounts.filter(item => item < 0).reduce((result, item) => (result += item), 0).toFixed(2);
+
+    balance.innerText = `฿` + numberWithCommas(total);
+    money_plus.innerText = `฿` + numberWithCommas(income);
+    money_minus.innerText = `฿` + numberWithCommas(expense);
+}
+
+function removeData(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id);
+    init(); // รีเฟรชข้อมูลใหม่
+}
+
+function addTransaction(e) {
+    e.preventDefault();
+    if (text.value.trim() === '' || amount.value.trim() === '') {
+        alert('กรุณาป้อนข้อมูลให้ครบ');
+    } else {
+        const data = {
+            id: autoID(),
+            text: text.value,
+            amount: +amount.value
+        };
+        transactions.push(data);
+        addDataToList(data);
+        calculeteMoney();
+        text.value = '';
+        amount.value = '';
+    }
+}
+
+form.addEventListener('submit', addTransaction);
 
 init();
